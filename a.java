408722b3368eb2bd829e7a416d26c94af33b7951
EC2 Default User <ec2-user@ip-172-31-19-17.us-east-2.compute.internal>
2022-11-15 12:58:25 +0000

    a.java modified by dev2

--- a.java
+++ a.java
@@ -1,3 +1,5 @@
 this is java file which is created on the branch 
 
+this is line added by dev2 in the main branch to check the git pull command
+
 this is the line which is added from the module3-branch
