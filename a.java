4a0c242b60e5facc7146f9c43d7a608f05cb8d2e
demouser <demouser@gmail.com>
2022-11-16 03:11:28 +0000

    test.py modified to testthe merge

--- a.java
+++ a.java
@@ -3,3 +3,4 @@ this is java file which is created on the branch
 this is line added by dev2 in the main branch to check the git pull command
 
 this is the line which is added from the module3-branch
+this is to test the stash
--- test.py
+++ test.py
@@ -8,3 +8,7 @@ response = requests.get('user_urs', auth=('username', 'password'))
 
 print(response)
 
+print('To give the xample of stash apply')
+
+print('some text')
+print('Added this line to test the merge')
