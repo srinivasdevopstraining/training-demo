2589a6daf2f901198ed897a496b7fd1c82dc4762
demouser <demouser@gmail.com>
2022-11-17 12:47:22 +0000

    b.java modified by dev3

--- b.java
+++ b.java
@@ -2,4 +2,4 @@ This is java file which created to the password less push to the github
 This is second line which is added to test ssh
 this is line which is addded by dev1 to the repo
 this is line which is added by dev2
-
+this is the line which is added by dev3 by feature/dev3 branch
