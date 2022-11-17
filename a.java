39499d57840de658935ac73d29a0437ad66e7de9
demouser <demouser@gmail.com>
2022-11-17 12:42:18 +0000

    a.java modified by dev2

--- a.java
+++ a.java
@@ -4,3 +4,4 @@ this is line added by dev2 in the main branch to check the git pull command
 
 this is the line which is added from the module3-branch
 this is to test the stash
+this is line which is added by dev2 from the feature/dev2 branch
